523b395bdc1c2830198ec2339fe7e13f068b0238
Cemilcan Gorur <goru0003@algonquinlive.com>
2025-04-20 08:46:44 -0400

    added project structure and task model

---
+++ ToDoApp/Models/Task.swift
@@ -0,0 +1,31 @@
+//
+//  Task.swift
+//  ToDoApp
+//
+//  Created by Cemilcan Gorur on 2025-04-19.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Task {
+    var id: UUID
+    var title: String
+    var notes: String?
+    var dueDate: Date?
+    var location: String?
+    var category: String?
+    var isDone: Bool
+ 
+    
+    init(title: String, notes: String? = nil, dueDate: Date? = nil, location: String? = nil, category: String? = nil, isDone: Bool = false) {
+        self.id = UUID()
+        self.title = title
+        self.notes = notes
+        self.dueDate = dueDate
+        self.location = location
+        self.category = category
+        self.isDone = isDone
+    }
+}
