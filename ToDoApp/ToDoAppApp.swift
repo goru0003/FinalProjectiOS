ba9ea94f2b16160b89f49d57763d0618343eb556
Cemilcan Gorur <goru0003@algonquinlive.com>
2025-04-20 08:41:37 -0400

    first commit

---
+++ ToDoApp/ToDoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoAppApp.swift
+//  ToDoApp
+//
+//  Created by Cemilcan Gorur on 2025-04-20.
+//
+
+import SwiftUI
+
+@main
+struct ToDoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
